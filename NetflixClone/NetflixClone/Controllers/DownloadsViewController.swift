ce619e1a1438eb4011c3deecff38c1df0655663d
Nikita Shakalov <nikitashakalov@gmail.com>
2023-09-09 19:58:51 +0300

    tab bar controller setup

--- NetflixClone/NetflixClone/Controllers/DownloadsViewController.swift
+++ NetflixClone/NetflixClone/Controllers/DownloadsViewController.swift
@@ -12,6 +12,6 @@ class DownloadsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .orange
+        view.backgroundColor = .systemBackground
     }
 }
