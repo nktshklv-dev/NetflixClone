9f744f531abcf4b804af3302455d1498f91200de
Nikita Shakalov <nikitashakalov@gmail.com>
2023-09-27 19:51:05 +0300

    some chnges in headerView add gradient layer add Play button add Download button

--- NetflixClone/NetflixClone/Views/HeroHeaderUIView.swift
+++ NetflixClone/NetflixClone/Views/HeroHeaderUIView.swift
@@ -9,6 +9,25 @@ import UIKit
 
 class HeroHeaderUIView: UIView {
 
+    private let playButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Play", for: .normal)
+        button.layer.borderColor = UIColor.white.cgColor
+        button.layer.borderWidth = 1
+        button.layer.cornerRadius = 5
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let downloadButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Download", for: .normal)
+        button.layer.borderColor = UIColor.white.cgColor
+        button.layer.borderWidth = 1
+        button.layer.cornerRadius = 5
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
     private let heroImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFill
@@ -20,13 +39,41 @@ class HeroHeaderUIView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         addSubview(heroImageView)
-        
-        
+        addGradient()
+        addSubview(playButton)
+        addSubview(downloadButton)
+        applyConstraints()
     }
     
+    private func addGradient() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [
+            UIColor.clear.cgColor,
+            UIColor.systemBackground.cgColor
+        ]
+        gradientLayer.frame = bounds
+        layer.addSublayer(gradientLayer)
+        
+    }
     override func layoutSubviews() {
         super.layoutSubviews()
         heroImageView.frame = self.bounds
+    }
+    
+    private func applyConstraints() {
+        let playButtonConstraints = [
+            playButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 60),
+            playButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -50),
+            playButton.widthAnchor.constraint(equalToConstant: 120)
+        ]
+        NSLayoutConstraint.activate(playButtonConstraints)
+        
+        let downloadButtonConstraints = [
+            downloadButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -60),
+            downloadButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -50),
+            downloadButton.widthAnchor.constraint(equalToConstant: 120)
+        ]
+        NSLayoutConstraint.activate(downloadButtonConstraints)
     }
     
     required init?(coder: NSCoder) {
