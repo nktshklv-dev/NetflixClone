f2dda344d78a5452eb175294949341aaf22f564f
Nikita Shakalov <nikitashakalov@gmail.com>
2023-09-27 22:17:53 +0300

    add netflix logo and some controls to the nav bar

--- NetflixClone/NetflixClone/Controllers/HomeViewController.swift
+++ NetflixClone/NetflixClone/Controllers/HomeViewController.swift
@@ -8,31 +8,49 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
     private let homeFeedTableView: UITableView = {
         let tableView = UITableView(frame: .zero, style: .grouped)
         tableView.register(CollectionViewTableViewCell.self, forCellReuseIdentifier: CollectionViewTableViewCell.identifier)
         return tableView
     }()
- 
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         view.addSubview(homeFeedTableView)
-        
+        configurateNavBar()
         homeFeedTableView.dataSource = self
         homeFeedTableView.delegate = self
         
         let headerView = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
         homeFeedTableView.tableHeaderView = headerView
     }
+    
+    private func configurateNavBar() {
+
+        let containerView = UIControl(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
+        let image = UIImageView(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
+        image.image = UIImage(named: "netflixLogo")
+        containerView.addSubview(image)
+        let logoBarButtonItem = UIBarButtonItem(customView: containerView)
+        logoBarButtonItem.width = 20
+        navigationItem.leftBarButtonItem = logoBarButtonItem
+        
+        navigationItem.rightBarButtonItems = [
+            UIBarButtonItem(image: UIImage(systemName: "person"), style: .done, target: self, action: nil),
+            UIBarButtonItem(image: UIImage(systemName: "play.rectangle"), style: .done, target: self, action: nil)]
+        navigationController?.navigationBar.tintColor = .white
+        
+        
+    }
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         homeFeedTableView.frame = view.bounds
     }
-
-
+    
+    
 }
 
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
