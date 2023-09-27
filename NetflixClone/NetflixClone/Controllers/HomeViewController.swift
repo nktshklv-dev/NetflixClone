b3e538263f600e3bad422211100fd64457e299ff
Nikita Shakalov <nikitashakalov@gmail.com>
2023-09-27 22:35:25 +0300

    made nav bar to stick to the top of the screen

--- NetflixClone/NetflixClone/Controllers/HomeViewController.swift
+++ NetflixClone/NetflixClone/Controllers/HomeViewController.swift
@@ -76,5 +76,12 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
         return 40
     }
     
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let defaultOffset = view.safeAreaInsets.top
+        let offset = scrollView.contentOffset.y + defaultOffset
+        
+        navigationController?.navigationBar.transform = .init(translationX: 0, y: min(0,-offset))
+    }
+    
     
 }
