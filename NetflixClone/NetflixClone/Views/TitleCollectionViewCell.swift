f53557f76cf370f197e567a5ab775e976654f787
nktshklv <nikitashakalov@gmail.com>
2023-11-02 13:56:13 +0300

    create TitleCollectionViewCell

---
+++ NetflixClone/NetflixClone/Views/TitleCollectionViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  TitleCollectionViewCell.swift
+//  NetflixClone
+//
+//  Created by Nikita Shakalov on 11/2/23.
+//
+
+import UIKit
+
+class TitleCollectionViewCell: UICollectionViewCell {
+    
+    static let id = "TitleCollectionViewCell"
+    
+    private let posterImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(posterImageView)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        posterImageView.frame = contentView.bounds
+    }
+    
+    
+}
