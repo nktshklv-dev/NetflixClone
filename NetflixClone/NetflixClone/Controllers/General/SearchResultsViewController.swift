d2b1e4c5433c3e308ab3bccbadc75639bbbe5ca5
nktshklv <nikitashakalov@gmail.com>
2023-11-22 02:09:06 +0300

    create file folders and new vc

---
+++ NetflixClone/NetflixClone/Controllers/General/SearchResultsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchResultsViewController.swift
+//  NetflixClone
+//
+//  Created by Nikita Shakalov on 11/22/23.
+//
+
+import UIKit
+
+class SearchResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGreen
+      
+    }
+
+}
